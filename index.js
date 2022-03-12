bcc8f065cda3164d57108d5db9f121795da820ba
Mike Pritchard <mike@arsenicsoup.com>
2022-03-12 14:13:48 -0500

    chore: re-org and tidy up

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 // Names exports
 module.exports = {
     DataTypes: require("./lib/types/index.js"),
+    Model: require("./lib/core/Model.js"),
     Engine: require("./lib/engines/Engine.js"),
     ClientEngine: require("./lib/engines/ClientEngine.js"),
-    Storm: require("./lib/Storm.js")
+    Storm: require("./lib/core/Storm.js")
 }   
  
